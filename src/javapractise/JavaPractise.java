dd8b0311f2251b033da618f34ef4c560a14f6d5f
MehreenJamil <mehreen.jamil1@gmail.com>
2018-11-06 17:29:33 +0100

    First time

---
+++ src/javapractise/JavaPractise.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractise;
+
+/**
+ *
+ * @author mehre
+ */
+import java.util.Scanner;
+
+public class JavaPractise {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+            }
+            System.out.println("================================");
+    }
+    
+}
